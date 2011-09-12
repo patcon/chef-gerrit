15fd161e0ddd7dbc8065ddaeba678001409e0a43
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-09-12 11:06:32 -0400

    Initial commit.

---
+++ recipes/default.rb
@@ -0,0 +1,110 @@
+#
+# Cookbook Name:: gerrit
+# Recipe:: default
+#
+# Copyright 2011, Myplanet Digital
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+user "gerrit2" do
+  uid "2345"
+  gid "gerrit2"
+  home "/home/gerrit2"
+  comment "Gerrit system user"
+  action :manage
+end
+
+remote_file "/home/gerrit2/gerrit.war" do
+  owner "gerrit2"
+  source "http://gerrit.googlecode.com/files/gerrit-#{node['gerrit']['version']}.war"
+end
+
+require_recipe "build-essential"
+require_recipe "mysql"
+require_recipe "mysql::server"
+require_recipe "database"
+
+mysql_connection_info = {
+    :host =>  "localhost",
+    :username => "root",
+    :password => node['mysql']['server_root_password']
+  }
+
+mysql_database "reviewdb" do
+  connection mysql_connection_info
+  action :create
+end
+
+mysql_database "changing the charset of reviewdb" do
+  connection mysql_connection_info
+  action :query
+  sql "ALTER DATABASE reviewdb charset=latin1"
+end
+
+mysql_database_user "gerrit2" do
+  connection mysql_connection_info
+  password node['mysql']['server_root_password']
+  action :create
+end
+
+mysql_database_user "gerrit2" do
+  connection mysql_connection_info
+  database_name "reviewdb"
+  privileges [
+    :all
+  ]
+  action :grant
+end
+
+mysql_database "flushing mysql privileges" do
+  connection mysql_connection_info
+  action :query
+  sql "FLUSH PRIVILEGES"
+end
+
+require_recipe "java"
+require_recipe "git"
+
+bash "Initializing Gerrit site" do
+  user "gerrit2"
+  group "gerrit2"
+  cwd "/home/gerrit2"
+  code <<-EOH
+  java -jar gerrit.war init -d review_site
+  EOH
+end
+
+#template "" do
+#  source
+#end
+
+bash "Starting gerrit daemon" do
+  user "gerrit2"
+  group "gerrit2"
+  code <<-EOH
+  ./home/gerrit2/review_site/bin/gerrit.sh start
+  EOH
+end
+
+link "/etc/init.d/gerrit.sh" do
+  to "/home/gerrit2/review_site/bin/gerrit.sh"
+end
+
+link "/etc/rc3.d/S90gerrit" do
+  to "../init.d/gerrit.sh"
+end
+
+#service "gerrit" do
+#end
