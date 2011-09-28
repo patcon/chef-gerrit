9df75460ae57107a0db3ab9c972aa776baf51c96
Brad Fritz <brad@fewerhassles.com>
2011-09-28 16:01:03 -0400

    import README from markdown file for long_description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Myplanet Digital"
 maintainer_email "patrick@myplanetdigital.com"
 license          "GNU Public License 3.0"
 description      "Installs/Configures gerrit"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w{ build-essential mysql database java git }.each do |cookbook|
